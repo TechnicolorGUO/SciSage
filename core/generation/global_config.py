5b31058afee7ca86e41a92f18a8b3af45056ac58
xiaofeng <30405889+xiaofengShi@users.noreply.github.com>
2025-07-18 14:21:26 +0800

    Update global_config.py

--- core/generation/global_config.py
+++ core/generation/global_config.py
@@ -2,9 +2,7 @@ import os
 import json
 import arxiv
 
-# GOOGLE_KEY = "36ad974e0dc97f3470762db318cd076f119a1474"  # shixf1989@gmail.com
-GOOGLE_KEY = "763472cad2f21aeb110715293b0c472efae149ae"  # jinxin
-GOOGLE_KEY = "1bcfd523ebf332cb3dcab96ca826cff9019ca4f8"  # 13521092651
+GOOGLE_KEY = "xxx"  # find here: 
 
 S2_API_KEY = ""  # semantic scholar api
 
@@ -22,15 +20,13 @@ retrival_model_url = "http://172.24.212.149:9655/v1/emb/encoder"
 # 召回服务
 # recall_server_url = recall_server_url
 
-recall_server_url = "http://172.24.214.62:5008/search"
+recall_server_url = "http://0.0.0.0:5008/search"
 # 排序模型
-rerank_model_url = "http://120.92.91.62:9756/v1/score/rerank"
+rerank_model_url = "http://0.0.0.0:9756/v1/score/rerank"
 
 # 核心chat模型
 chat_system = "You are a helpful AI assistant for scientific literature review. Please carefully follow user's instruction and help them to understand the most recent papers."
 
-# chat_url = f"http://120.92.91.62:9757/v1/chat/completions" ## openscholar
-chat_url = "http://120.92.91.62:9089/v1/chat/completions"  # r1-70b
+chat_url = "http://0.0.0.0:9089/v1/chat/completions"  # r1-70b
 
-# CHAT_MODEL_NAME = "r1-llama70b"
-CHAT_MODEL_NAME = "openscholar"
+CHAT_MODEL_NAME = "r1-llama70b"
